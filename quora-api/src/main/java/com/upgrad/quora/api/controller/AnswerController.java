11379eab3c3ea7dbfb5e478c945c3343d9c79314
goyalrishab <rishab959290@gmail.com>
2020-05-04 23:05:55 +0530

    Added endpoint answer/all/{questionId}

--- quora-api/src/main/java/com/upgrad/quora/api/controller/AnswerController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/AnswerController.java
@@ -14,7 +14,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.ZonedDateTime;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/")
@@ -66,6 +68,22 @@ public class AnswerController {
         return new ResponseEntity<>(answerDeleteResponse,HttpStatus.OK);
     }
 
+    @GetMapping("answer/all/{questionId}")
+    public ResponseEntity<List<AnswerDetailsResponse>> getAllAnswersToQuestion(@PathVariable("questionId") final String questionId, @RequestHeader("authorization") final String accessToken) throws AuthorizationFailedException, InvalidQuestionException {
+
+        List<AnswerEntity> answerEntities =  answerBusinessService.getAllAnswersByQuestionId(questionId, accessToken);
+
+        List<AnswerDetailsResponse> answerDetailsResponseList = answerEntities.stream()
+                .map(answer -> {
+                    AnswerDetailsResponse answerDetailsResponse = new AnswerDetailsResponse();
+                    answerDetailsResponse.setId(answer.getUuid());
+                    answerDetailsResponse.setAnswerContent(answer.getContent());
+                    answerDetailsResponse.setQuestionContent(answer.getQuestion().getContent());
+                    return answerDetailsResponse;
+                }).collect(Collectors.toList());
+
+        return new ResponseEntity<>(answerDetailsResponseList, HttpStatus.OK);
+    }
 
 }
 
