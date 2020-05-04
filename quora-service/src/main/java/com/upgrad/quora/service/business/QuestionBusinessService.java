9da41b8ed8c8f6263ce8e9a58054b4e70fefc7c9
Vibhushit07 <vibhushitsinghal80@gmail.com>
2020-05-04 12:48:08 +0530

    Added Question Business Service

---
+++ quora-service/src/main/java/com/upgrad/quora/service/business/QuestionBusinessService.java
@@ -0,0 +1,34 @@
+package com.upgrad.quora.service.business;
+
+import com.upgrad.quora.service.dao.QuestionDao;
+import com.upgrad.quora.service.dao.UserDao;
+import com.upgrad.quora.service.entity.QuestionEntity;
+import com.upgrad.quora.service.entity.UserAuthenticationTokenEntity;
+import com.upgrad.quora.service.exception.AuthorizationFailedException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class QuestionBusinessService {
+
+    @Autowired
+    private QuestionDao questionDao;
+
+    @Autowired
+    private UserDao userDao;
+
+    public QuestionEntity createQuestion(final QuestionEntity questionEntity, final String authorizationToken) throws AuthorizationFailedException {
+
+        UserAuthenticationTokenEntity userAuthenticationTokenEntity = questionDao.getUserAuthToken(authorizationToken);
+
+        if (userAuthenticationTokenEntity == null)
+            throw new AuthorizationFailedException("ATHR-001", "User has not signed in");
+
+        if (userAuthenticationTokenEntity.getLogoutAt() != null)
+            throw new AuthorizationFailedException("ATHR-002", "User is signed out.Sign in first to post a question");
+
+        questionEntity.setUserEntity(userAuthenticationTokenEntity.getUserEntity());
+        return questionDao.createQuestion(questionEntity);
+    }
+}
