2d3974d0eb476979b7577d1eed16f08d2ff35359
Vibhushit07 <vibhushitsinghal80@gmail.com>
2020-05-04 13:01:12 +0530

    Added Endpoint /question/all

--- quora-api/src/main/java/com/upgrad/quora/api/controller/QuestionController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/QuestionController.java
@@ -12,7 +12,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.ZonedDateTime;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 
 @RestController
@@ -36,5 +38,22 @@ public class QuestionController {
         QuestionResponse questionResponse = new QuestionResponse().id(createdquestionEntity.getUuid()).status("QUESTION CREATED");
 
         return new ResponseEntity<QuestionResponse>(questionResponse, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/question/all")
+    public ResponseEntity<List<QuestionDetailsResponse>> getAllQuestions(@RequestHeader("authorization") final String authorization) throws AuthorizationFailedException {
+
+        List<QuestionEntity> list = questionBusinessService.getAllQuestions(authorization);
+
+        List<QuestionDetailsResponse> questionDetailsResponseList = list.stream()
+                .map(questions -> {
+                    QuestionDetailsResponse q = new QuestionDetailsResponse();
+                    q.setContent(questions.getContent());
+                    q.setId(questions.getUuid());
+                    return q;
+                }).collect(Collectors.toList());
+
+        return new ResponseEntity<>(questionDetailsResponseList, HttpStatus.OK);
+        
     }
 }
